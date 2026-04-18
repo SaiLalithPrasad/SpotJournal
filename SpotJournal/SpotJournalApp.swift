f2bcf207221a1b0c7ec9059b6215dc697e22cc8f
SaiLalithPrasad <sailalithprasad.pathi@gmail.com>
2026-04-18 08:54:45 -0700

    Initial Commit

---
+++ SpotJournal/SpotJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotJournalApp.swift
+//  SpotJournal
+//
+//  Created by Lalith Prasad on 4/18/26.
+//
+
+import SwiftUI
+
+@main
+struct SpotJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
